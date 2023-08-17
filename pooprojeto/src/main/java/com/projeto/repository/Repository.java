c9b77f164804b8731555cf57ef3a7c31f78064c5
jvistorscosta <jvictorscosta@gmail.com>
2023-08-17 18:44:25 -0300

    Implementação de classes abstratas

--- pooprojeto/src/main/java/com/projeto/repository/Repository.java
+++ pooprojeto/src/main/java/com/projeto/repository/Repository.java
@@ -1,5 +1,26 @@
 package com.projeto.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Repository {
-    public
+    private List<Object> list;
+
+    public Repository() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(Object entrada) {
+        list.add(entrada);
+    }
+    public Object get(int index){
+        if(index >= 0 && index < list.size()){
+            return list.get(index);
+        }
+        return null;
+    }
+    public List<Object> getAll() {
+        return new ArrayList<>(list);
+    }
+
 }
