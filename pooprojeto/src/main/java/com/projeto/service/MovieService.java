9d14fc16e66d13d2c67eab8e39dcf5d5de6d1ea7
Helen Her <her.helen@gmail.com>
2023-08-17 23:01:30 -0300

    adicionando implementação inicial do MovieService

---
+++ pooprojeto/src/main/java/com/projeto/service/MovieService.java
@@ -0,0 +1,51 @@
+package com.projeto.service;
+
+import com.projeto.domain.Movie;
+import com.projeto.repository.MovieRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MovieService {
+    private static int COUNTER_MOVIE_ID;
+    private MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public void create(String name, LocalDate releaseDate, double budget, String description) {
+        this.movieRepository.add(new Movie(this.COUNTER_MOVIE_ID++, name, releaseDate, budget, description));
+    }
+
+    public Movie find(String name) {
+        return this.movieRepository.getByName(name);
+    }
+
+    public List<Movie> listAll() {
+        return this.movieRepository.getAll()
+                .stream()
+                .filter(element -> element instanceof Movie)
+                .map(element -> (Movie) element)
+                .collect(Collectors.toList());
+    }
+
+
+    public void associateActor() {
+
+    }
+
+    public void associateDirector() {
+
+    }
+
+    public void associateProducer() {
+
+    }
+
+    public void associateScreenwriter() {
+
+    }
+
+}
